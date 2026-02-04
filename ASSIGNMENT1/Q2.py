0bc6ba5aad416b64dafe37868cdb0286c93e56e0
Khushi Tomar <khushit.ip.24@nitj.ac.in>
2026-02-04 11:45:32 +0530

    Update Q2.py

--- ASSIGNMENT1/Q2.py
+++ ASSIGNMENT1/Q2.py
@@ -1,17 +1,18 @@
 import numpy as np
 import pandas as p
 
-r=int(input("enter no of rows"))
-c=int(input("enter no of columns"))
+r=int(input("Enter no of rows "))
+c=int(input("Enter no of columns "))
 a=[]
 
+print("Enter elements")
 for i in range(r):
     row=[]
     for j in range(c):
         row.append(int(input()))
     a.append(row)
 
-print("\noriginal list:\n",a)
+print("\nOriginal list:\n",a)
 
 np_2a=np.array(a)
 print("\n2d numpy array:\n",np_2a)
@@ -20,12 +21,8 @@ np_a=np_2a.flatten()
 print("\n1d numpy array:\n",np_a)
 
 pd_series = p.Series(np_a)
-print("\nPandas Series:\n",pd_series)
+print("\nPandas Series:\n")
+for i, val in enumerate(pd_series):
+    print(f"{i} -> {val}")
 
-p_series=p.Series(a)
-print("\npandas series:\n",p_series)
-
-df=p.DataFrame(np_2a)
-p_1d=df.stack()
-print("\n1d panda array:\n",p_1d)
 
